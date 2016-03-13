4463301ec2b27a5de5ecebb84a52174c1d61ba68
John Gramila <tynanjk@gmail.com>
2016-03-13 15:53:42 -0500

    Updated config/locales/en.yml

--- app/models/rest_list.rb
+++ app/models/rest_list.rb
@@ -1,9 +1,11 @@
 class RestList < ActiveRecord::Base
-  validates :rest_name, presence: true
-  validates :phone_num, presence: true
-  validates :street_address, presence: true
-  validates :city, presence: true
-  validates :state, presence: true
-  validates :zipcode, presence: true
+  validates :rest_name, presence: true, message: "Restaurant Name is a Required Field"
+  validates :phone_num, presence: true, message: "Phone Number is a Required Field"
+  validates_format_of :phone_num, :with => /\A(\+1|1)?([2-9]\d\d[2-9]\d{6})\z/, message: 'This number isn\'t formatted properly, try it like 555-555-5555'
+  validates :street_address, presence: true, message: "Street Address is a Required Field"
+  validates :city, presence: true, message: "City is a Required Field"
+  validates :state, presence: true, message: "State is a Required Field"
+  validates :zipcode, presence: true, message: "Zip Code is a Required Field"
   STATES = ['AL', 'AK', 'AS', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'DC', 'FM', 'FL', 'GA', 'GU', 'HI', 'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 'LA', 'ME', 'MH', 'MD', 'MA', 'MI', 'MN', 'MS', 'MO', 'MT', 'NE', 'NV', 'NH', 'NJ', 'NM', 'NY', 'NC', 'ND', 'MP', 'OH', 'OK', 'OR', 'PW', 'PA', 'PR', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT', 'VT', 'VI', 'VA', 'WA', 'WV', 'WI', 'WY', 'AE', 'AA', 'AP']
+  belongs_to :owner
 end
